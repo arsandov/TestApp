5dcd0a8db2d84da829b65557829df988ac2dd31e
damandak <damandak@uc.cl>
2014-09-07 17:43:34 -0300

    Estructura RoR creada

---
+++ TestApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestApp::Application.config.secret_key_base = '41603d5e944bf11ead845911c797f406e3bf05911770f157fa9a88e4be4a1fa78845ac497b9312b27ef4c87917cb3f59ac0a066d0a87b0e6f410ac7c46daff5e'
