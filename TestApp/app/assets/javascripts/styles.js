00497f51329fd5a0a031f722b91d6268497217d4
Pedro Agustin Hojas Loret <pedrohojas@gmail.com>
2014-09-20 16:05:29 -0300

    animate

--- TestApp/app/assets/javascripts/styles.js
+++ TestApp/app/assets/javascripts/styles.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
     $('.ingresar').on('keyup','.textInput',function(){
         var tagFilter ='.' + $(this).val();
-        $('.post').removeClass('high');
-        $('.post').filter(tagFilter).addClass('high');
+        $('.post').addClass('hide');
+        $('.post').filter(tagFilter).removeClass('hide');
+    });
+});
+$(document).ready(function(){
+    $('.change').on('click',function(){
+       $('#image_box').attr('src',$(this).closest('.post').data('image'));
+    });
+});
+$(document).ready(function(){
+    $('#animate').on('click',function(){
+        $('#image_box').addClass($( "#animation_box" ).val()+" animated");
     });
 });
--- TestApp/db/migrate/20140915233927_create_archivos.rb
+++
@@ -1,8 +0,0 @@
-class CreateArchivos < ActiveRecord::Migration
-  def change
-    create_table :archivos do |t|
-
-      t.timestamps
-    end
-  end
-end
